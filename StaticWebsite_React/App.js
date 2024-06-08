e44510fa40f7faf8da7bd0a9bec91c550dfa1ab2
akankshadaware <110918241+akankshadaware@users.noreply.github.com>
2024-06-08 10:53:54 +0530

    Create App.js

---
+++ StaticWebsite_React/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Header/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
